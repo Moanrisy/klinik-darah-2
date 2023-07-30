6c8d2f6f59e09beb6d9381b4014804e1648fd81b
Moanrisy <moanrisy@gmail.com>
2023-07-30 12:58:07 +0700

    feat: Add popup to show bank to transfer

--- app/Views/payment_order.php
+++ app/Views/payment_order.php
@@ -51,9 +51,38 @@
                 <div class="col">
                     <h4>Total Price</h4>
                     <p><strong>Total:</strong> <?php echo '$' . number_format($order['total_price'], 2); ?></p>
+
+                    <!-- Add a button to open the payment modal -->
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#paymentModal">Proceed to Pay</button>
+
                 </div>
             </div>
 
+        </div>
+    </div>
+</div>
+
+<!-- Add this div outside the 'content' section to create the modal -->
+<div class="modal fade" id="paymentModal" tabindex="-1" aria-labelledby="paymentModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="paymentModalLabel">Payment Details</h5>
+                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <h3>Silahkan transfer ke bank berikut</h3>
+                <br>
+                <p><strong>Bank Name:</strong> BRI</p>
+                <p><strong>Account Number:</strong> 11112345838</p>
+                <p><strong>Recipient Name:</strong> PT. Klinik Darah</p>
+                <!-- Add other bank details as needed -->
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <!-- You can handle the payment processing when this button is clicked -->
+                <button type="button" class="btn btn-primary" onclick="proceedToPay()">Proceed to Pay</button>
+            </div>
         </div>
     </div>
 </div>
